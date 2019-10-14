02da53182ea19a0cdf13806468880c7b3cc0817b
Zach Leatherman <zachleatherman@gmail.com>
2019-10-14 16:34:10 -0500

    Sold copy

--- index.11ty.js
+++ index.11ty.js
@@ -5,7 +5,7 @@ class Index {
 		};
 	}
 
-	render() {
+	getSignupHtml() {
 		return `
 <form method="get" action="/failure/" class="msb-register" id="msb-register">
 	<div class="msb-register-fields">
@@ -19,6 +19,23 @@ class Index {
 	</div>
 </form>
 `;
+	}
+
+	getSoldHtml() {
+		return `
+<div class="msb-register" id="msb-register">
+	<div class="msb-register-fields">
+		<p class="msb-emoji">👩🏻‍🚀👨🏿‍🚀👨🏾‍🚀👨🏽‍🚀👨🏼‍🚀👨🏻‍🚀👩🏿‍🚀👩🏾‍🚀👩🏽‍🚀👩🏼‍🚀</p>
+		<h2>It’s been an incredible journey!</h2>
+		<p class="msb-seeya">We are happy to announce that effective immediately, myspacebook will be ending. Any content associated with your account will be deleted. We couldn’t be happier about this transition. Thank you for supporting us and being a part of this incredible journey!</p>
+	</div>
+</div>
+`;
+	}
+
+
+	render() {
+		return this.getSoldHtml();
 	}
 }
 
